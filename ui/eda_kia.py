50b778765a04a5bb71899de4c346cdd4e4384033
dmdals1012 <dmdals1012@gmail.com>
2025-03-28 15:28:15 +0900

    Update eda_kia.py

--- ui/eda_kia.py
+++ ui/eda_kia.py
@@ -7,6 +7,7 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import os
 
 # 한글 폰트 설정
 plt.rcParams['font.family'] = 'Malgun Gothic'
